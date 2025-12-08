b9ca4b6ee3b19281ff05d9a426ef8bb7512c3c4e
sayjoy97 <sayjjoy97@gmail.com>
2025-12-08 16:20:53 +0900

    sayjoy97 작업

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import {useState} from "react";
+import {Routes, Route} from "react-router-dom";
 
 import "./App.css";
 import Header from "./layouts/header/Header";
 import Home from "./pages/Home/Home";
 import PlantManage from "./pages/PlantManage/PlantManage";
 // import Market from "./pages/Market/Market";
-// import MyPage from "./pages/MyPage/MyPage";
+import MyPage from "./pages/MyPage/MyPage";
+import MyPageView from "./pages/MyPage/MyPageView";
+import MyPageEdit from "./pages/MyPage/MyPageEdit";
+import MyPageTimelapse from "./pages/MyPage/MyPageTimelapse";
 import Login from "./pages/Login/Login";
 
 function App() {
@@ -25,8 +28,16 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/plants" element={<PlantManage />} />
-        {/* <Route path="/market" element={<Market />} />
-        <Route path="/mypage" element={<MyPage />} /> */}
+        {/* <Route path="/market" element={<Market />} /> */}
+
+        {/* 마이페이지 (부모) */}
+        <Route path="/mypage" element={<MyPage />}>
+          <Route index element={<MyPageView />} /> {/* 기본 페이지 */}
+          <Route path="view" element={<MyPageView />} />
+          <Route path="edit" element={<MyPageEdit />} />
+          <Route path="timelapse" element={<MyPageTimelapse />} />
+        </Route>
+
         <Route path="/login" element={<Login onLogin={setUser} />} />
       </Routes>
     </>
