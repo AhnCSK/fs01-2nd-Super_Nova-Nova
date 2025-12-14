bd2b854472fd1b8d8db86d8bd18fc33b368664a2
eddy0417 <eddy0417@naver.com>
2025-12-15 01:43:10 +0900

    가입 부분 수정, 충돌 수정, 민감 수정, jwt, security2 뺀 버전 찐

--- frontend/src/pages/MyPage/MyPage.jsx
+++ frontend/src/pages/MyPage/MyPage.jsx
@@ -1,14 +1,14 @@
 import "./MyPage.css";
-
-import {Outlet} from "react-router-dom";
-import {useEffect, useState} from "react";
-// import {getUserInfo} from "../../api/mypage/mypageAPI";
+import { useAuth } from "../../api/auth/AuthContext";
+import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getUserInfo } from "../../api/mypage/mypageAPI";
 
 function MyPage(userId) {
   // userId 상위에서 받아오는 값
   // 토큰에서 분리해서 가져올 예정
   const mockUserId = 1; // 우선 mock데이터 사용
-
+  const { user } = useAuth();
   const [userInfo, setUserInfo] = useState({
     usersResponseDTO: {
       userId: "",
@@ -24,7 +24,7 @@ function MyPage(userId) {
     novaResponseDTOList: [],
   });
   useEffect(() => {
-    getUserInfo(mockUserId)
+    getUserInfo(user.userId)
       .then((data) => {
         setUserInfo(data);
         console.log(data);
@@ -38,7 +38,7 @@ function MyPage(userId) {
     <div className="mypage-wrapper">
       <div className="mypage-container">
         {/* 서브페이지가 이곳에 렌더링됨 */}
-        <Outlet context={{userInfo, setUserInfo}} />
+        <Outlet context={{ userInfo, setUserInfo }} />
       </div>
     </div>
   );
